3dda7b69bc8254ce95ee39b1865ac8edb562ea45
hamid <code1404.le@gmail.com>
2025-11-01 19:20:39 +0330

    Edited admin panel
    Edited admin panel.

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from blog.models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'published_date', 'created_date', 'updated_date')
+    search_fields = ('title', 'content')
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_filter = ('status',)
+    # ordering = ('-created_date',)
+
+    
 
--- website/models.py
+++ website/models.py
@@ -10,7 +10,11 @@ class Contact(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_date',]
 
+    def __str__(self):
+        return f'{self.name} - {self.id}'
 
 
 
