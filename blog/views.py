1de26a649e1ab35fbd6553027419bbd8294f40b6
hamid <code1404.le@gmail.com>
2025-11-05 23:30:50 +0330

    used database in project
    Edited views.py and blog-home.html and used data from database in template.

--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,15 @@ from django.http import HttpResponse
 from blog.models import Post
 
 def BlogHome_view(request):
-    return render(request , 'blog\\blog-home.html')
+    posts = Post.objects.filter(status=True)
+    context = {'posts' : posts}
+    return render(request , 'blog\\blog-home.html', context)
 
 def BlogSingle_view(request):
     return render(request , 'blog\\blog-single.html')
 
 def test_view(request):
-    publish_posts = Post.objects.filter(status=True)
-    private_posts = Post.objects.filter(status=False)
-    context = {'publish_posts' : publish_posts , 'private_posts' : private_posts}
-    return render(request , 'blog\\test.html' , context)
+    
+    return render(request , 'blog\\test.html' )
 
 
