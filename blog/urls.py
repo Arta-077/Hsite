7733f04271ca3d97af9efa3b5d3f243896883577
admin <code1404.le@gmail.com>
2025-12-18 00:53:20 +0330

    Search sidebar & Paginator
    + Paginator for Posts
    + Search sidebar in content posts

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,9 @@ app_name = 'blog'
 urlpatterns = [
     path('home' , BlogHome_view , name='homeblog'),
     path('<int:pid>' , BlogSingle_view , name='singleblog'),
-    path('category/<str:cat_name>' , BlogCategory_view , name='categoryblog'),
+    path('category/<str:cat_name>' , BlogHome_view , name='categoryblog'),
+    path('author/<str:author_username>' , BlogHome_view , name='authorblog'),
+    path('search/' , BlogSearch_view , name='searchblog'),
     # path('<int:pid>' , test_view , name='testblog'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,26 @@
 
 from django.shortcuts import render , get_object_or_404
-
-
 from django.http import HttpResponse
 from blog.models import Post
+from django.core.paginator import Paginator , EmptyPage, PageNotAnInteger
 
-def BlogHome_view(request):
+def BlogHome_view(request, cat_name=None, author_username=None):
     posts = Post.objects.filter(status=True)
+
+    if cat_name:
+        posts = Post.objects.filter(status=True, category__name=cat_name)
+    if author_username:
+        posts = Post.objects.filter(status=True, author__username=author_username)
+
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    
     context = {'posts' : posts}
     return render(request , 'blog\\blog-home.html', context)
 
@@ -25,7 +39,16 @@ def test_view(request,pid):
 
 def BlogCategory_view(request,cat_name):
     posts = Post.objects.filter(status=True , category__name=cat_name)
+
     context = {'posts' : posts}
     return render(request , 'blog\\blog-home.html', context)
 
 
+def BlogSearch_view(request):
+    posts = Post.objects.filter(status=True) 
+
+    if s := request.GET.get('search'): 
+        posts = posts.filter(content__icontains=s) 
+
+    context = {'posts' : posts}
+    return render(request , 'blog/blog-home.html', context)
