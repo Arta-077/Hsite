ac8d6503c73319df9350d24ea2a18df1c1e863df
admin <code1404.le@gmail.com>
2025-12-30 18:57:20 +0330

    Several modules were added to the project
    + Django summernote
    + Django simple captcha
    + Django multi captcha admin
    Added Summernote  for context field.
    Added captcha for form & admin login.

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-
 from blog.models import Post , Category
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('id' ,'title', 'author' , 'status', 'published_date', 'created_date', 'updated_date')
     search_fields = ('title', 'content')
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     list_filter = ('status',)
     # ordering = ('-created_date',)
+    summernote_fields = ('content',)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
